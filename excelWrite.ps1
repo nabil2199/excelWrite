70d08ba47bccddf3806d54190b82f6153f30e9de
nabil2199 <nabil2199@gmail.com>
2016-12-22 12:19:49 +0100

    files

--- excelWrite.ps1
+++ excelWrite.ps1
@@ -1,10 +1,26 @@
-$ExcelObject = New-Object -ComObject Excel.Application
-$ExcelObject.visible = $false
-$ExcelObject.DisplayAlerts = $false
-$date = Get-Date -Format "yyyyMMddHHss"
-$strPath1 = "o:\UserCert\Active_Users_$date.xlsx"
-if (Test-Path $strPath1) {
-  #Open the document  
-  $ActiveWorkbook = $ExcelObject.WorkBooks.Open($strPath1)
-  $ActiveWorksheet = $ActiveWorkbook.Worksheets.Item(1)
+
+$file = "C:\Repositories\excelWrite\ExcelFile.xlsx"
+$sheetName = "Sheet1"
+$objExcel = New-Object -ComObject Excel.Application
+$workbook = $objExcel.Workbooks.Open($file)
+$sheet = $workbook.Worksheets.Item($sheetName)
+$objExcel.Visible=$false
+#Count max row
+$rowMax = ($sheet.UsedRange.Rows).count 
+#Declare the starting positions
+$rowName,$colName = 1,1
+$rowAge,$colAge = 1,2
+$rowCity,$colCity = 1,3
+#loop to get values and store it
+for ($i=1; $i -le $rowMax-1; $i++)
+{
+$name = $sheet.Cells.Item($rowName+$i,$colName).text 
+$age = $sheet.Cells.Item($rowAge+$i,$colAge).text 
+$city = $sheet.Cells.Item($rowCity+$i,$colCity).text 
+
+Write-Host ("My Name is: "+$name)
+Write-Host ("My Age is: "+$age)
+Write-Host ("I live in: "+$city)
 }
+#close excel file
+$objExcel.quit()
